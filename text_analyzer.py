4b1f15012b0511666f8a93c6aa872f2c7662bde1
patzez <pat.zezulka@gmail.com>
2022-04-13 10:08:05 +0200

    Graf cetnosti delek slov

--- text_analyzer.py
+++ text_analyzer.py
@@ -1,7 +1,11 @@
-'''
-author =
-'''
-TEXTS = ['''
+"""
+text_analyzer.py: První projekt do Engeto Online Python Akademie
+
+author: Patrik Zezulka
+email: pat.zezulka@gmail.com
+discord: Patrik Z.#8128
+"""
+TEXTS = ["""
 Situated about 10 miles west of Kemmerer,
 Fossil Butte is a ruggedly impressive
 topographic feature that rises sharply
@@ -9,16 +13,20 @@ some 1000 feet above Twin Creek Valley
 to an elevation of more than 7500 feet
 above sea level. The butte is located just
 north of US 30N and the Union Pacific Railroad,
-which traverse the valley. ''',
-'''At the base of Fossil Butte are the bright
+which traverse the valley. 
+""",
+"""
+At the base of Fossil Butte are the bright
 red, purple, yellow and gray beds of the Wasatch
 Formation. Eroded portions of these horizontal
 beds slope gradually upward from the valley floor
 and steepen abruptly. Overlying them and extending
 to the top of the butte are the much steeper
 buff-to-white beds of the Green River Formation,
-which are about 300 feet thick.''',
-'''The monument contains 8198 acres and protects
+which are about 300 feet thick.
+""",
+"""
+The monument contains 8198 acres and protects
 a portion of the largest deposit of freshwater fish
 fossils in the world. The richest fossil fish deposits
 are found in multiple limestone layers, which lie some
@@ -26,8 +34,8 @@ are found in multiple limestone layers, which lie some
 represent several varieties of perch, as well as
 other freshwater genera and herring similar to those
 in modern oceans. Other fish such as paddlefish,
-garpike and stingray are also present.'''
-]
+garpike and stingray are also present.
+"""]
 separator = "-" * 40
 registred_users = {"bob": "123", "ann": "pass123",
                    "mike": "password123", "liz": "pass123"}
@@ -92,3 +100,21 @@ else:
           f"There are {numeric} numeric strings.",
           f"The sum of all the numbers is {numeric_sum}.",
           separator, sep="\n")
+
+# Graf cetnosti delek slov:
+occurences = {}
+
+for length in words:
+    if len(length) not in occurences:
+        occurences[len(length)] = 1
+    else:
+        occurences[len(length)] += 1
+
+print("LEN|    OCCURENCES    |NR. ",
+      separator, sep="\n")
+for key in sorted(occurences):
+    stars = occurences[key] * "*"
+    print(f"{key:>3}|{stars:<17} |{occurences[key]}",
+          sep="\n")
+else:
+    print(separator)
